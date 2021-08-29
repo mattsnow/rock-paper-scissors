f0b2fcd81ac016ccdd5b5276e203a8e06369444f
Matt <oystermenator@gmail.com>
2021-08-29 12:31:32 +0100

    First UI revision

--- scripts.js
+++ scripts.js
@@ -1,7 +1,76 @@
-let computerScore = 0
-let humanScore = 0
+// Top container
+const container = document.querySelector('#container');
+const table = document.createElement('div');
+table.classList.add('table');
+table.style.border = ("thick dashed black");
+table.style.padding = "10px 10px 300px 10px";
+table.style.margin = "50px 300px 50px 300px";
+table.style.background = "#8dd6ae";
+container.appendChild(table);
 
-function computerPlay() {
+// Results paragraph
+const results = document.createElement('p');
+results.classList.add('results');
+results.id = "results";
+table.appendChild(results);
+
+// Bottom container
+const controls = document.createElement('div');
+controls.classList.add('controls');
+controls.style.border = ("thick dashed black");
+controls.style.padding = "10px";
+controls.style.margin = "50px 300px 50px 300px";
+container.appendChild(controls);
+
+// Container for Start button
+const startDiv = document.createElement('div');
+startDiv.classList.add('startDiv');
+startDiv.style.padding = "10px 10px 50px 10px";
+startDiv.style.margin = "10px 580px";
+controls.appendChild(startDiv);
+
+// Container for player choice buttons
+const inputDiv = document.createElement('div');
+inputDiv.classList.add('inputDiv');
+inputDiv.style.padding = "50px";
+inputDiv.style.margin = "10px 100px";
+controls.appendChild(inputDiv);
+
+// Buttons
+// var button = document.createElement("button");
+// button.innerHTML = "Start Game";
+// button.style.padding = "25px";
+// button.style.fontSize = "large"
+// button.id = "start";
+// startDiv.appendChild(button);
+
+var button = document.createElement("button");
+button.innerHTML = "Rock";
+button.style.padding = "25px";
+button.style.fontSize = "large"
+button.style.margin = "0px 25px";
+button.id = "rock";
+inputDiv.appendChild(button);
+
+var button = document.createElement("button");
+button.innerHTML = "Paper";
+button.style.padding = "25px";
+button.style.fontSize = "large"
+button.style.margin = "0px 25px";
+button.id = "paper";
+inputDiv.appendChild(button);
+
+var button = document.createElement("button");
+button.innerHTML = "Scissors";
+button.style.padding = "25px";
+button.style.fontSize = "large"
+button.style.margin = "0px 25px";
+button.id = "scissors";
+inputDiv.appendChild(button);
+
+// Functions
+
+function generateComputerPlay() {
     let result
     let number = Math.random()
 
@@ -13,51 +82,36 @@ function computerPlay() {
         result = "scissors";
     }
 
-    console.log("Computer plays " + result)
-
     return (result)
 }
 
-function userInput() {
-    let person = prompt("Please enter your play", "Rock");
-
-    person = person.toLowerCase();
-
-    if ((person == "rock") || (person == "paper") || (person == "scissors")) {
-        console.log("You play " + person)
-        return (person)
-    } else {
-        return (null)
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
+function calculateRound(playerSelection, computerSelection) {
 
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
             return ("It's a draw!")
         } else if (computerSelection == "paper") {
-            return ("You lose! Paper beats rock")
+            return ("You lose! Paper beats rock.")
         } else {
-            return ("You win! Rock beats scissors")
+            return ("You win! Rock beats scissors.")
         }
     }
 
     if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            return ("You win! Paper beats rock")
+            return ("You win! Paper beats rock.")
         } else if (computerSelection == "paper") {
             return ("It's a draw!")
         } else {
-            return ("You lose! Scissors beats paper")
+            return ("You lose! Scissors beats paper.")
         }
     }
 
     if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
-            return ("You lose! Rock beats scissors")
+            return ("You lose! Rock beats scissors.")
         } else if (computerSelection == "paper") {
-            return ("You win! Scissors beats paper")
+            return ("You win! Scissors beats paper.")
         } else {
             return ("It's a draw!")
         }
@@ -76,13 +130,43 @@ function tallyScore(game_result_str) {
     }
 }
 
-function playGame() {
-    let game_result
+function playRound(person) {
 
-    console.log(`Round 1: `)
-    game_result = playRound(userInput(), computerPlay());
+
+
+    game_result = calculateRound(person, computersPlay);
     tallyScore(game_result.slice(0, 5))
-    console.log(game_result)
-    console.log("Computer: " + computerScore + " Human: " + humanScore)
-    console.log("\n")
+
+}
+
+// Main code
+
+document.getElementById("rock").addEventListener("click", playGame.bind(null, "rock"), false);
+document.getElementById("paper").addEventListener("click", playGame.bind(null, "paper"), false);
+document.getElementById("scissors").addEventListener("click", playGame.bind(null, "scissors"), false);
+// document.getElementById("start").addEventListener("click", playGame);
+
+let computerScore = 0;
+let humanScore = 0;
+let game_result;
+const outputResults = document.getElementById('results');
+let computersPlay = generateComputerPlay();
+let i = 0;
+
+function playGame(person) {
+
+    if (i < 5) {
+
+        computersPlay = generateComputerPlay()
+        playRound(person);
+        outputResults.innerHTML = `You play ${person}. Computer plays ${computersPlay}. ${game_result} Computer: ${computerScore} Human: ${humanScore}`;
+        i++;
+    } else {
+
+        if (computerScore > humanScore) {outputResults.innerHTML = `The game is over. The final score is Computer: ${computerScore} Human: ${humanScore}. The computer wins!`;}
+        else if (computerScore < humanScore) {outputResults.innerHTML = `The game is over. The final score is Computer: ${computerScore} Human: ${humanScore}. You win!`;}
+        else {outputResults.innerHTML = `The game is over. The final score is Computer: ${computerScore} Human: ${humanScore}. It's a draw!`;}
+
+    }
+
 }
