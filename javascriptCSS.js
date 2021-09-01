d81b1387b8178376d9d35d999e2e583a4d217e92
Matt <oystermenator@gmail.com>
2021-09-01 10:21:37 +0100

    UI based on Flexbox

---
+++ javascriptCSS.js
@@ -0,0 +1,100 @@
+// var elem = document.createElement('div');
+// elem.style.cssText = `
+// font-size: 40px;
+// min-height: 500px;
+// width: 100%;
+// background: LightGray;
+// display: grid;
+// grid-template-columns: 1fr 1fr 1fr;
+// grid-template-rows: 1fr 1fr 1fr;
+// grid-gap: 10px;
+// grid-template-areas: "play play play" "l controls r" "rock paper scissors";
+// `;
+// elem.id = "container";
+// document.body.appendChild(elem);
+
+// Results paragraph
+// var item1 = document.createElement('div');
+// item1.style.cssText = `background:LightSkyBlue; grid-area: play; justify-self: center; align-self: center;`;
+// item1.id = "item1";
+// container.appendChild(item1);
+
+// var item1 = document.createElement('div');
+// item1.style.cssText = `
+// background:LightSkyBlue;
+// grid-area: play; 
+// justify-self: center; 
+// align-self: center;
+// `;
+// elem.class = "class1"; 
+// document.container.appendChild(item1);
+
+// test insert into <div>
+// const container = document.querySelector('#container');
+// const table = document.createElement('div');
+// table.classList.add('table');
+// table.style.border = ("thick dashed black");
+// table.style.padding = "10px 10px 300px 10px";
+// table.style.margin = "50px 300px 50px 300px";
+// table.style.background = "#8dd6ae";
+// container.appendChild(table);
+
+// // Results paragraph
+// const results = document.createElement('p');
+// results.classList.add('results');
+// results.id = "results";
+// table.appendChild(results);
+
+// // Bottom container
+// const controls = document.createElement('div');
+// controls.classList.add('controls');
+// controls.style.border = ("thick dashed black");
+// controls.style.padding = "10px";
+// controls.style.margin = "50px 300px 50px 300px";
+// container.appendChild(controls);
+
+// // Container for Start button
+// const startDiv = document.createElement('div');
+// startDiv.classList.add('startDiv');
+// startDiv.style.padding = "10px 10px 50px 10px";
+// startDiv.style.margin = "10px 580px";
+// controls.appendChild(startDiv);
+
+// // Container for player choice buttons
+// const inputDiv = document.createElement('div');
+// inputDiv.classList.add('inputDiv');
+// inputDiv.style.padding = "50px";
+// inputDiv.style.margin = "10px 100px";
+// controls.appendChild(inputDiv);
+
+// // Buttons
+// // var button = document.createElement("button");
+// // button.innerHTML = "Start Game";
+// // button.style.padding = "25px";
+// // button.style.fontSize = "large"
+// // button.id = "start";
+// // startDiv.appendChild(button);
+
+// var button = document.createElement("button");
+// button.innerHTML = "Rock";
+// button.style.padding = "25px";
+// button.style.fontSize = "large"
+// button.style.margin = "0px 25px";
+// button.id = "rock";
+// item3.appendChild(button);
+
+// var button = document.createElement("button");
+// button.innerHTML = "Paper";
+// button.style.padding = "25px";
+// button.style.fontSize = "large"
+// button.style.margin = "0px 25px";
+// button.id = "paper";
+// inputDiv.appendChild(button);
+
+// var button = document.createElement("button");
+// button.innerHTML = "Scissors";
+// button.style.padding = "25px";
+// button.style.fontSize = "large"
+// button.style.margin = "0px 25px";
+// button.id = "scissors";
+// inputDiv.appendChild(button);
--- scripts.js
+++ scripts.js
@@ -1,73 +1,3 @@
-// Top container
-const container = document.querySelector('#container');
-const table = document.createElement('div');
-table.classList.add('table');
-table.style.border = ("thick dashed black");
-table.style.padding = "10px 10px 300px 10px";
-table.style.margin = "50px 300px 50px 300px";
-table.style.background = "#8dd6ae";
-container.appendChild(table);
-
-// Results paragraph
-const results = document.createElement('p');
-results.classList.add('results');
-results.id = "results";
-table.appendChild(results);
-
-// Bottom container
-const controls = document.createElement('div');
-controls.classList.add('controls');
-controls.style.border = ("thick dashed black");
-controls.style.padding = "10px";
-controls.style.margin = "50px 300px 50px 300px";
-container.appendChild(controls);
-
-// Container for Start button
-const startDiv = document.createElement('div');
-startDiv.classList.add('startDiv');
-startDiv.style.padding = "10px 10px 50px 10px";
-startDiv.style.margin = "10px 580px";
-controls.appendChild(startDiv);
-
-// Container for player choice buttons
-const inputDiv = document.createElement('div');
-inputDiv.classList.add('inputDiv');
-inputDiv.style.padding = "50px";
-inputDiv.style.margin = "10px 100px";
-controls.appendChild(inputDiv);
-
-// Buttons
-// var button = document.createElement("button");
-// button.innerHTML = "Start Game";
-// button.style.padding = "25px";
-// button.style.fontSize = "large"
-// button.id = "start";
-// startDiv.appendChild(button);
-
-var button = document.createElement("button");
-button.innerHTML = "Rock";
-button.style.padding = "25px";
-button.style.fontSize = "large"
-button.style.margin = "0px 25px";
-button.id = "rock";
-inputDiv.appendChild(button);
-
-var button = document.createElement("button");
-button.innerHTML = "Paper";
-button.style.padding = "25px";
-button.style.fontSize = "large"
-button.style.margin = "0px 25px";
-button.id = "paper";
-inputDiv.appendChild(button);
-
-var button = document.createElement("button");
-button.innerHTML = "Scissors";
-button.style.padding = "25px";
-button.style.fontSize = "large"
-button.style.margin = "0px 25px";
-button.id = "scissors";
-inputDiv.appendChild(button);
-
 // Functions
 
 function generateComputerPlay() {
@@ -132,11 +62,17 @@ function tallyScore(game_result_str) {
 
 function playRound(person) {
 
-
-
     game_result = calculateRound(person, computersPlay);
     tallyScore(game_result.slice(0, 5))
+}
 
+function resetGame() {
+
+    i = 0;
+    computerScore = 0;
+    humanScore = 0;
+    outputResults.innerHTML = "Choose your selection to begin";
+    outputScore.innerHTML = "There are 5 rounds";
 }
 
 // Main code
@@ -144,12 +80,13 @@ function playRound(person) {
 document.getElementById("rock").addEventListener("click", playGame.bind(null, "rock"), false);
 document.getElementById("paper").addEventListener("click", playGame.bind(null, "paper"), false);
 document.getElementById("scissors").addEventListener("click", playGame.bind(null, "scissors"), false);
-// document.getElementById("start").addEventListener("click", playGame);
+document.getElementById("reset").addEventListener("click", resetGame);
 
 let computerScore = 0;
 let humanScore = 0;
 let game_result;
-const outputResults = document.getElementById('results');
+const outputResults = document.getElementById('text1');
+const outputScore = document.getElementById('text2');
 let computersPlay = generateComputerPlay();
 let i = 0;
 
@@ -159,14 +96,21 @@ function playGame(person) {
 
         computersPlay = generateComputerPlay()
         playRound(person);
-        outputResults.innerHTML = `You play ${person}. Computer plays ${computersPlay}. ${game_result} Computer: ${computerScore} Human: ${humanScore}`;
+        outputResults.innerHTML = `You play ${person}. Computer plays ${computersPlay}. ${game_result}`;
+        outputScore.innerHTML = `Computer: ${computerScore} Human: ${humanScore}`;
         i++;
+
     } else {
 
-        if (computerScore > humanScore) {outputResults.innerHTML = `The game is over. The final score is Computer: ${computerScore} Human: ${humanScore}. The computer wins!`;}
-        else if (computerScore < humanScore) {outputResults.innerHTML = `The game is over. The final score is Computer: ${computerScore} Human: ${humanScore}. You win!`;}
-        else {outputResults.innerHTML = `The game is over. The final score is Computer: ${computerScore} Human: ${humanScore}. It's a draw!`;}
-
+        if (computerScore > humanScore) {
+            outputResults.innerHTML = `The game is over. The final score is:`;
+            outputScore.innerHTML = `Computer: ${computerScore} Human: ${humanScore}. The computer wins!`
+        } else if (computerScore < humanScore) {
+            outputResults.innerHTML = `The game is over. The final score is:`;
+            outputScore.innerHTML = ` Computer: ${computerScore} Human: ${humanScore}. You win!`
+        } else {
+            outputResults.innerHTML = `The game is over. The final score is:`;
+            outputScore.innerHTML = ` Computer: ${computerScore} Human: ${humanScore}. It's a draw!`
+        }
     }
-
 }
